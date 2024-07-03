6f195ec3183c4ee8f2bb05bf48536c132a6cf73f
milanesiluca <luca.milanesi@live.se>
2024-07-03 15:17:13 +0200

    last bug fixed

--- JS/calcolator.js
+++ JS/calcolator.js
@@ -4,53 +4,44 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 function isNumber(value) {
-    return typeof value === 'number';
+    return typeof Number(value) === 'number';
 }
 
-console.log("write 'calculator' to start the calculator");
-
-function calculator(){
-
-    console.log("Available operation:");
-    console.log("'+' - Addiction");
-    console.log("'-' - Substraction");
-    console.log("'*' - Multiplication");
-    console.log("'/' - Division");
 
     
 
-    const choice = prompt("Insert your choice");
+    const choice = prompt("Insert the choosen operation (+, -, *, /)");
     if (!['+', '-', '*', '/'].includes(choice) ){
-        console.log("Invalid choice. Please write 'calculator' to start a new operation");
-        return;
+        alert("Invalid choice.");
+        
     }
 
-    const a = prompt("insert the first number: ");
+    let a = prompt("insert the first number: ");
     if (isNumber(a) == false){
-        console.log("Invalid input. Please write 'calculator' to start a new operation");
-        return;
-    }
+        alert("Invalid input");
+        //return;
+    } else 
+        a = Number(a);
 
-    const b = prompt("insert the second number: ");
+    let b = prompt("insert the second number: ");
     if (isNumber(b) == false){
-        console.log("Invalid input. Please write 'calculator' to start a new operation");
-        return;
-    }
+        alert("Invalid input");
+        //return;
+    } else
+        b = Number(b);
 
     if (choice === '+'){
-        add(a, b);
+        alert(`result ${add(a, b)}`);;
     } else if (choice === '-'){
-        substruction(a, b);
+        alert(`result ${substruction(a, b)}`);
     } else if (choice === '*'){
-        multiply(a, b);
-    } else {
-        if (a === 0)
-            return 0;
-        else if (b == 0){
-            console.log("A number cannot be diveded for 0");
-            return;
+        alert(`result ${multiply(a, b)}`);
+    } else if (choice === '/'){
+        if (b == 0){
+            alert("A number cannot be diveded for 0");
+            //return;
         } else
-            divide(a, b);
-    }
+            alert(`result ${divide(a, b)}`);
+    } else
+        alert("operation impossible to calculate");
 
-}
